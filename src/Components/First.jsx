d3b8d35cfd4a1156195c0cfd497506e832d320c7
Abdul Malik <167233705+NizAdal@users.noreply.github.com>
2025-04-03 10:42:50 +0500

    Update First.jsx

--- src/Components/First.jsx
+++ src/Components/First.jsx
@@ -9,12 +9,7 @@ function First() {
   useEffect(() => {
     const getArticals = async () => {
       try {
-        const adal = await axios.get("https://newsapi.org/v2/top-headlines", {
-  params: {
-    country: "us",
-    apiKey: process.env.REACT_APP_NEWS_API_KEY, // Use the variable here
-  },
-});
+        const adal = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_NEWS_API_KEY}`);
         setArtical(adal.data.articles);
       } catch (error) {
         console.error("Error fetching articles:", error);
